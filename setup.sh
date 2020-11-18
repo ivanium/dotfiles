03b1f1b7317963be056ffa68f9b01e632ea8cbb0
Yifan Qiao <yifanqiao@ucla.edu>
2020-11-18 12:51:49 -0700

    update zsh syntax highlighting

--- setup.sh
+++ setup.sh
@@ -30,6 +30,8 @@ backup_if_exists $HOME/.gitignore_global
 cp zsh/zshrc $HOME/.zshrc
 tar xzf zsh/oh-my-zsh.tar.gz
 cp -r oh-my-zsh/.oh-my-zsh $HOME/.oh-my-zsh
+rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp zsh/p10k.zsh $HOME/.p10k.zsh
 
 cp tmux/tmux.conf $HOME/.tmux.conf
